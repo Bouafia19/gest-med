5516913a5c3a2d6501827a84d86bff3b414ffa3a
Bouafia19 <khaledsetif1900@gmail.com>
2020-03-22 19:48:25 +0100

    fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcomes/leaflet'
   devise_for :users
   resources :users
   root 'welcomes#index'
-
-  get 'welcomes/leaflet'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
